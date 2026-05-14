f10215aedb9e1a455e6041c0f6d0541361718cb5
MMA <mohomabdullah@gmail.com>
2026-05-14 11:33:36 +0300

    feat: add API v1 versioning structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import healthRoutes from "./routes/health.routes.js";
+import v1Router from "./api/v1/index.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/", healthRoutes);
+app.get("/", (req,res) => {
+  res.send("Care Lounge Api Running");
+});
+
+app.use("/api/v1",v1Router);
 
 export default app;
